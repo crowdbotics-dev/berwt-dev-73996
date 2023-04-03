2c65611234521c32a325c214ed6558a9930a9f78
Crowdbotics <engineering@crowdbotics.com>
2023-04-03 17:17:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/berwt_dev_73996/MainActivity.java
@@ -0,0 +1,15 @@
+package com.berwt_dev_73996;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "berwt_dev_73996";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "berwt-dev-73996",
+  url: "https://berwt-dev-73996.botics.co",
+  api: "https://berwt-dev-73996.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
